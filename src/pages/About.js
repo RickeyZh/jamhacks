aace34421bcd177dfa9a5284340d05acdb2bbd75
RickeyZh <rickey.zhengg@gmail.com>
2024-06-08 10:45:03 -0400

    Added about section content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,14 @@
 export default function About(){
     return(
-        <div>Aboat</div>
+        <div>
+            <div className="Header">About</div>
+            <div className="Content">
+
+            <b>GripGuardian</b> is an innovative solution designed to help developers maintain healthy hand positions, prevent strain, and enhance overall productivity. Our custom-designed ergonomic mouse integrates advanced sensors to monitor hand positions and detect stress points, identifying risks for conditions such as carpal tunnel syndrome, arthritis, and other repetitive strain injuries. By continuously analyzing hand positions and pressure points, <b>GripGuardian</b> provides real-time feedback and alerts to encourage healthier hand postures.
+            <br/><br/>
+            Paired with a comprehensive web application, <b>GripGuardian</b> offers detailed analytics and personalized tips to users. The interactive platform displays hand position data, stress indicators, and usage patterns, helping developers understand their habits and make informed adjustments. With proactive ergonomic solutions, ErgonomicGuard aims to reduce the incidence of repetitive strain injuries, fostering a healthier and more productive developer community.
+            
+            </div>
+        </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
-import { Link } from "react-router-dom";
-
 export default function Home() {
   return (
-    <>aaiuyiuyiuyiuya</>
+    <div>
+      <div className="title"> Hello</div>
+    </div>
   );
 }
