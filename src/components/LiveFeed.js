5ecc2891b4cf012fc21e660d45d2a1df2cc64cb1
Eric Chen <70295495+cloudec11@users.noreply.github.com>
2024-06-08 09:36:23 -0400

    more basic stuff

---
+++ src/components/LiveFeed.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function LiveFeed() {
+  return (
+    <h1>
+
+    Real Time Metrics
+        
+    </h1>
+    
+
+  );
+}
---
+++ src/pages/Monitor.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function Monitor() {
+  return (
+    <h1>
+
+    Real Time Metrics
+        
+    </h1>
+    
+
+  );
+}
