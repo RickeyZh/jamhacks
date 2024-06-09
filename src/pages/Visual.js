b83fbac7cf64eb6a3bf7f49f0b089377b28826af
Andrew Yeung <71157672+Andrew-Yeung@users.noreply.github.com>
2024-06-08 21:22:40 -0400

    Added a few images

--- src/pages/Visual.js
+++ src/pages/Visual.js
@@ -1,4 +1,15 @@
 import "../App.css";
 export default function Visual() {
-  return <div>Visual</div>;
+  return(
+
+
+    <div style={{ backgroundColor: '#3d516e', minHeight: '100vh', padding: '0px' }}>
+            <div className="AboutHeader">Visual</div>
+            <div className="Content" style={{color:'white'}}>
+            <img src="https://www.shutterstock.com/image-photo/anonymous-person-touching-his-wrist-600nw-2367133597.jpg" alt="React Image" />
+
+        
+            </div>
+        </div>
+  )
 }
